10c79c2e177343c1a926dfeed8360b7dacc8ff8c
tknemuru <kojikoji.max.5252@gmail.com>
2025-01-01 13:16:22 +0900

    add FileListBuilder.

---
+++ Memories/Builders/FileListBuilder.cs
@@ -0,0 +1,48 @@
+﻿using Memories.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Memories.Builders
+{
+    /// <summary>
+    /// ファイルリストの構築機能を提供します。
+    /// </summary>
+    public class FileListBuilder : IBuildable
+    {
+        /// <summary>
+        /// メタデータのリスト
+        /// </summary>
+        private IEnumerable<MovieFileMetadata> metadatas;
+
+        /// <summary>
+        /// コンストラクタ
+        /// </summary>
+        public FileListBuilder()
+        {
+            metadatas = new List<MovieFileMetadata>();
+        }
+
+        public FileListBuilder SetMetadatas(IEnumerable<MovieFileMetadata> _metadatas)
+        {
+            metadatas = _metadatas;
+            return this;
+        }
+
+        /// <summary>
+        /// ファイルリストの文字列を構築します。
+        /// </summary>
+        /// <returns></returns>
+        public string Build()
+        {
+            var builder = new StringBuilder();
+            foreach (var metadata in metadatas)
+            {
+                builder.AppendLine($"file '{metadata.FileName}'");
+            }
+            return builder.ToString();
+        }
+    }
+}
