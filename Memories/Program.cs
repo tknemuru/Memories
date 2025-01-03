abda4491308cbbd35b6f84b895fd8856cfe875ad
tknemuru <kojikoji.max.5252@gmail.com>
2025-01-04 06:58:14 +0900

    add error catch.

--- Memories/Program.cs
+++ Memories/Program.cs
@@ -1,5 +1,6 @@
 ﻿using Memories;
 using Memories.Executors;
+using Memories.Helplers;
 using Memories.Models;
 
 public class Program
@@ -7,40 +8,50 @@ public class Program
 #pragma warning disable CS8604
     static void Main(string[] args)
     {
-        AppConfig.Init();
-
-        // 引数無しの場合は動画作成を実行する
-        var mode = ExeMode.CreateMovie;
-
-        if (args != null && args.Length > 0)
+        try
         {
-            mode = (ExeMode)int.Parse(args[0]);
-        }
+            AppConfig.Init();
 
-        switch (mode)
+            // 引数無しの場合は動画作成を実行する
+            var mode = ExeMode.CreateMovie;
+
+            if (args != null && args.Length > 0)
+            {
+                mode = (ExeMode)int.Parse(args[0]);
+            }
+
+            switch (mode)
+            {
+                case ExeMode.CreateMovie:
+                    // 作成件数指定無しの場合は1件作成する
+                    var count = 1;
+                    if (args != null && args.Length > 1)
+                    {
+                        // 複数件作成したい場合は第二引数で件数を指定する
+                        count = int.Parse(args[1]);
+                    }
+
+                    FileHelper.Log($"動画作成件数：{count}件");
+                    var creator = new MovieCreator();
+                    for (var i = 0; i < count; i++)
+                    {
+                        FileHelper.Log($"{i + 1}件目の動画作成開始");
+                        creator.Create();
+                        FileHelper.Log($"{i + 1}件目の動画作成完了");
+                    }
+                    break;
+                case ExeMode.SendNoticeMail:
+                    if (args == null || args.Length < 2)
+                    {
+                        throw new ArgumentException("通知メール送信は必ず第二引数に送信先アドレスを指定してください。");
+                    }
+                    new MailSender().Send(args[1]);
+                    break;
+            }
+        } catch (Exception ex)
         {
-            case ExeMode.CreateMovie:
-                // 作成件数指定無しの場合は1件作成する
-                var count = 1;
-                if (args != null && args.Length > 1)
-                {
-                    // 複数件作成したい場合は第二引数で件数を指定する
-                    count = int.Parse(args[1]);
-                }
-
-                var creator = new MovieCreator();
-                for (var i = 0; i < count; i++)
-                {
-                    creator.Create();
-                }
-                break;
-            case ExeMode.SendNoticeMail:
-                if (args == null || args.Length < 2)
-                {
-                    throw new ArgumentException("通知メール送信は必ず第二引数に送信先アドレスを指定してください。");
-                }
-                new MailSender().Send(args[1]);
-                break;
+            FileHelper.Log($"エラーが発生しました。{ex}");
+            throw;
         }
     }
 }
